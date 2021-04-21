460ff98dea98516dc9391aa9a394f04b8413e04b
Matt Umland <mattumland@gmail.com>
2021-04-21 12:04:36 -0400

    Add preliminary data file

---
+++ src/data/gdData.js
@@ -0,0 +1,770 @@
+export const floors = {
+  'theBell': {
+    'encounters': {
+      01: {
+        'description': 'Arkady making repairs',
+        'reference': 'pg. 17',
+        'count': 0 ,
+        'tags': ''
+      },
+      02: {
+        'description': 'Arkady making repairs',
+        'reference': 'pg. 17',
+        'count': 0 ,
+        'tags': ''
+      },
+      03: {
+        'description': 'Arkady making repairs',
+        'reference': 'pg. 17',
+        'count': 0 ,
+        'tags': ''
+      },
+      04: {
+        'description': 'Noriko on prayer walk',
+        'reference': 'pg. 17',
+        'count': 0 ,
+        'tags': ''
+      },
+      05: {
+        'description': 'Noriko on prayer walk',
+        'reference': 'pg. 17',
+        'count': 0 ,
+        'tags': ''
+      },
+      06: {
+        'description': 'Noriko on prayer walk',
+        'reference': 'pg. 17',
+        'count': 0 ,
+        'tags': ''
+      },
+      07: {
+        'description': 'Random new Mercenary for hire arrives',
+        'reference':'' ,
+        'count': 0,
+        'tags': 'name'
+      08: {
+        'description': 'Random new Mercenary for hire arrives',
+        'reference':'' ,
+        'count': 0,
+        'tags': 'name'
+      },
+      09: {
+        'description': 'New Diver arrives',
+        'reference':'' ,
+        'count': 0,
+        'tags': 'name'
+      },
+      10: {
+        'description': 'Messenger from Commander Kilroy',
+        'reference':'pg. 15' ,
+        'count': 0,
+        'tags': 'name'
+      }
+    }
+  },
+  'floor1': {
+    'encounters': {
+      01: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.5',
+        'tags': 'forgotten'
+      02: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.5',
+        'tags': 'forgotten'
+      03: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.5',
+        'tags': 'forgotten'
+      },
+      04: {
+        'description': 'Distorted corporate muzak plays over the intercom',
+        'reference':'',
+        'count': 0,
+        'tags': ''
+      05: {
+        'description': 'Distorted corporate muzak plays over the intercom',
+        'reference':'',
+        'count': 0,
+        'tags': ''
+      },
+      06: {
+        'description': 'Distorted corporate muzak plays over the intercom',
+        'reference':'',
+        'count': 0,
+        'tags': ''
+      },
+      07: {
+        'description': 'A Security Android',
+        'reference':'pg. 10',
+        'count': 0,
+        'tags': ''
+      },
+      08: {
+        'description': 'A Security Android',
+        'reference':'pg. 10',
+        'count': 0,
+        'tags': ''
+      },
+      09: {
+        'description': 'A Diver' ,
+        'reference':'',
+        'count': 0 ,
+        'tags': 'name'
+      },
+      10: {
+        'description': 'Troubleshooters' ,
+        'reference': 'pg. 14',
+        'count': '1.10',
+        'tags': 'name'
+    }
+  },
+  'floor2': {
+    'encounters': {
+      01: {
+        'description': 'Chosen Androids',
+        'reference': 'pg. 24',
+        'count': '1.5',
+        'tags': ''
+      },
+      02: {
+        'description': 'Chosen Androids',
+        'reference': 'pg. 24',
+        'count': '1.5',
+        'tags': ''
+      },
+      03: {
+        'description': 'Chosen Androids',
+        'reference': 'pg. 24',
+        'count': '1.5',
+        'tags': ''
+      },
+      04: {
+        'description': 'Fallen Androids',
+        'reference': 'pg. 24',
+        'count': '1.10',
+        'tags': ''
+      },
+      05: {
+        'description': 'Fallen Androids',
+        'reference': 'pg. 24',
+        'count': '1.10',
+        'tags': ''
+      06: {
+        'description': 'Fallen Androids',
+        'reference': 'pg. 24',
+        'count': '1.10',
+        'tags': ''
+      07: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      08: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      09: {
+        'description': 'A Diver' ,
+        'reference':'',
+        'count': 0 ,
+        'tags': 'name'
+      },
+      10: {
+        'description': 'The Minotaur',
+        'reference': 'pg. 29' ,
+        'count': 0,
+        'tags':
+      }
+    }
+  },
+  'floor3.1': {
+    'encounters': {
+      01: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      02: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      03: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      04: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      05: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      06: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      07: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      08: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      09: {
+        'description': 'A Diver' ,
+        'reference':'',
+        'count': 0 ,
+        'tags': 'name'
+      },
+      10: {
+        'description': 'Waste Disposal Body Save or 1d10 DMG' ,
+        'reference':'',
+        'count': 0,
+        'tags':
+      }
+    }
+  },
+  'floor3.2': {
+    'encounters': {
+      01: {
+        'description': 'Bee Drones' ,
+        'reference': 'pg. 38',
+        'count': '2.10',
+        'tags': ''
+      },
+      02: {
+        'description': 'Bee Drones' ,
+        'reference': 'pg. 38',
+        'count': '2.10',
+        'tags': ''
+      },
+      03: {
+        'description': 'Bee Drones' ,
+        'reference': 'pg. 38',
+        'count': '2.10',
+        'tags': ''
+      },
+      04: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      05: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      06: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      07: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      08: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      09: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      10: {
+        'description': 'A Diver' ,
+        'reference':'',
+        'count': 0 ,
+        'tags': 'name'
+      }
+    }
+  },
+  'floor3.3': {
+    'encounters': {
+      01: {
+        'description': 'Silus Makes Demands',
+        'reference': 'pg. 40' ,
+        'count': 0,
+        'tags': 'silus'
+      },
+      02: {
+        'description': 'Silus Makes Demands',
+        'reference': 'pg. 40' ,
+        'count': 0,
+        'tags': 'silus'
+      03: {
+        'description': 'Silus Makes Demands',
+        'reference': 'pg. 40' ,
+        'count': 0,
+        'tags': 'silus'
+      },
+      04: {
+        'description': 'Pseudomilk Eels' ,
+        'reference': 'pg. 42',
+        'count': '1.10',
+        'tags': ''
+      },
+      05: {
+        'description': 'Pseudomilk Eels' ,
+        'reference': 'pg. 42',
+        'count': '1.10',
+        'tags': ''
+      },
+      06: {
+        'description': 'Pseudomilk Eels' ,
+        'reference': 'pg. 42',
+        'count': '1.10',
+        'tags': ''
+      },
+      07: {
+        'description': 'Stunted Androids',
+        'reference':'pg. 41',
+        'count': '1.5'
+        'tags':''
+      },
+      08: {
+        'description': 'Stunted Androids',
+        'reference':'pg. 41',
+        'count': '1.5'
+        'tags':''
+      },
+      09: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      10: {
+        'description': 'A Diver' ,
+        'reference':'',
+        'count': 0 ,
+        'tags': 'name'
+      }
+    }
+  },
+  'floor3.4': {
+    'encounters': {
+      01: {
+        'description': 'Escaped Child Androids from HEL seeking the Mind Thief' ,
+        'reference': 'pg. 45',
+        'count': '1.5',
+        'tags': ''
+      },
+      02: {
+        'description': 'Escaped Child Androids from HEL seeking the Mind Thief' ,
+        'reference': 'pg. 45',
+        'count': '1.5',
+        'tags': ''
+      },
+      03: {
+        'description': 'Escaped Child Androids from HEL seeking the Mind Thief' ,
+        'reference': 'pg. 45',
+        'count': '1.5',
+        'tags': ''
+      },
+      04: {
+        'description': 'The Mind Thief',
+        'reference': 'pg. 45' ,
+        'count': 0,
+        'tags': ''
+      },
+      05: {
+        'description': 'The Mind Thief',
+        'reference': 'pg. 45' ,
+        'count': 0,
+        'tags': ''
+      },
+      06: {
+        'description': 'The Mind Thief',
+        'reference': 'pg. 45' ,
+        'count': 0,
+        'tags': ''
+      },
+      07: {
+        'description': 'Android Dogs'
+        'reference': 'pg. 44',
+        'count':' 1.5' ,
+        'tags':''
+      },
+      08: {
+        'description': 'Android Dogs'
+        'reference': 'pg. 44',
+        'count':' 1.5' ,
+        'tags':''
+      },
+      09: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      10: {
+        'description': 'A Diver' ,
+        'reference':'',
+        'count': 0 ,
+        'tags': 'name'
+      }
+    }
+  },
+  'floor3.5': {
+    'encounters': {
+      01: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      02: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      03: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      04: {
+        'description': 'Failed Androids looking to escape Disassembly ',
+        'reference': 'pg. 49',
+        'count': '1.5',
+        'tags': ''
+      },
+      05: {
+        'description': 'Failed Androids looking to escape Disassembly ',
+        'reference': 'pg. 49',
+        'count': '1.5',
+        'tags': ''
+      },
+      06: {
+        'description': 'Failed Androids looking to escape Disassembly ',
+        'reference': 'pg. 49',
+        'count': '1.5',
+        'tags': ''
+      },
+      07: {
+        'description': 'Security Androids chasing escapees',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      08: {
+        'description': 'Security Androids chasing escapees',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      09: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      10: {
+        'description': 'A Diver' ,
+        'reference':'',
+        'count': 0 ,
+        'tags': 'name'
+      }
+    }
+  },
+  'floor3.6': {
+    'encounters': {
+      01: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      02: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      03: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.10',
+        'tags': 'forgotten'
+      },
+      04: {
+        'description': 'Failed Androids looking to escape Disassembly ',
+        'reference': 'pg. 49',
+        'count': '1.10',
+        'tags': ''
+      },
+      05: {
+        'description': 'Failed Androids looking to escape Disassembly ',
+        'reference': 'pg. 49',
+        'count': '1.10',
+        'tags': ''
+      },
+      06: {
+        'description': 'Failed Androids looking to escape Disassembly ',
+        'reference': 'pg. 49',
+        'count': '1.10',
+        'tags': ''
+      },
+      07: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      08: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      09: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      10: {
+        'description': 'A Diver' ,
+        'reference':'',
+        'count': 0 ,
+        'tags': 'name'
+      }
+    }
+  },
+  'floor3.7': {
+    'encounters': {
+      01: {
+        'description': 'Divers',
+        'reference': '',
+        'count': '1.5',
+        'tags': 'name'
+      },
+      02: {
+        'description': 'Divers',
+        'reference': '',
+        'count': '1.5',
+        'tags': 'name'
+      },
+      03: {
+        'description': 'Divers',
+        'reference': '',
+        'count': '1.5',
+        'tags': 'name'
+      },
+      04: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      05: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      06: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.10',
+        'tags': ''
+      },
+      07: {
+        'description': 'Infiltrator Android of NPC the crew as already met',
+        'reference': 'pg. 10',
+        'count': 0 ,
+        'tags': ''
+      },
+      08: {
+        'description': 'Infiltrator Android of NPC the crew as already met',
+        'reference': 'pg. 10',
+        'count': 0 ,
+        'tags': ''
+      },
+      09: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      10: {
+        'description': 'Exact duplicates of the crew (+1d10 Bends & Bends Check)',
+        'reference': 'pg. 10',
+        'count': 0,
+        'tags': ''
+      }
+    }
+  },
+  'floor4': {
+    'encounters': {
+      01: {
+        'description': 'Escaped Child Androids' ,
+        'reference': 'pg. 54',
+        'count': '1.10',
+        'tags': 'child'
+      },
+      02: {
+        'description': 'Escaped Child Androids' ,
+        'reference': 'pg. 54',
+        'count': '1.10',
+        'tags': 'child'
+      },
+      03: {
+        'description': 'Escaped Child Androids' ,
+        'reference': 'pg. 54',
+        'count': '1.10',
+        'tags': 'child'
+      },
+      04: {
+        'description': 'Former CLOUDBANK Research Scientists',
+        'reference': '',
+        'count': '1.5',
+        'tags': 'name'
+      },
+      05: {
+        'description': 'Former CLOUDBANK Research Scientists',
+        'reference': '',
+        'count': '1.5',
+        'tags': 'name'
+      },
+      06: {
+        'description': 'Former CLOUDBANK Research Scientists',
+        'reference': '',
+        'count': '1.5',
+        'tags': 'name'
+      },
+      07: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.5',
+        'tags': ''
+      },
+      08: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.5',
+        'tags': ''
+      },
+      09: {
+        'description': 'Ghost in the Machine',
+        'reference': 'pg. 12',
+        'count': 0,
+        'tags': 'ghost'
+      },
+      10: {
+        'description': 'A Diver' ,
+        'reference':'',
+        'count': 0 ,
+        'tags': 'name'
+      }
+      }
+    }
+  },
+  'floor6': {
+    'encounters': {
+      01: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.5',
+        'tags': 'forgotten'
+      },
+      02: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.5',
+        'tags': 'forgotten'
+      },
+      03: {
+        'description': 'Forgotten Androids',
+        'reference': 'pg. 11' ,
+        'count': '1.5',
+        'tags': 'forgotten'
+      },
+      04: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.5',
+        'tags': ''
+      },
+      05: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.5',
+        'tags': ''
+      },
+      06: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.5',
+        'tags': ''
+      },
+      07: {
+        'description': 'Security Androids',
+        'reference':'pg. 10',
+        'count': '1.5',
+        'tags': ''
+      },
+      08: {
+        'description': 'The Hunter',
+        'reference': 'pg. 57',
+        'count': 0,
+        'tags': ''
+      },
+      09: {
+        'description': 'The Hunter',
+        'reference': 'pg. 57',
+        'count': 0,
+        'tags': ''
+      },
+      10: {
+        'description': 'Troubleshooters' ,
+        'reference': 'pg. 14',
+        'count': '1.10',
+        'tags': 'name'
+      }
+    }
+  }
+}
+/*additionalTables
+  forgotten androids
+  troubleshooter details
+  silus makes demands
+  ghost in the machine
+  cryopods
+
+*/
