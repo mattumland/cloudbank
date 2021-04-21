51d87d71648c0dfbe06ad318a09822ee17f60d37
Matt Umland <mattumland@gmail.com>
2021-04-21 18:38:13 -0400

    Add basic component structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
+import Header from '../Header/Header';
+import Floor from '../Floor/Floor';
+import { floors } from '../data/gdData';
+
 
 const App = () => {
+  
 
   return (
 
