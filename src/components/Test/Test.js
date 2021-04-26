d20eafce4e1435f051b20782ee48d5b7e73158db
Matt Umland <mattumland@gmail.com>
2021-04-26 13:37:20 -0400

    Add complete test component

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,7 +1,33 @@
-import React from 'react';
-import { addTags } from '../utilities';
+import React, { useState } from 'react';
+import { addTags } from '../../utilities';
+import { fetchName } from '../../data/apiCaller'
+import './Test.scss';
 
-const Test = () => {
+const Test = ({ encounter }) => {
+  const [singleName, setName] = useState('')
+
+  const testFetch = (nameID) => {
+
+  Promise.all([fetchName(nameID)])
+    .then((nameData) => {
+      setName(`'${nameData[0].character.name}'`)
+    })
+    .catch(err => setName(`'Mummy'`))
+  }
+
+
+  addTags(encounter); //produces a random name
+  testFetch('CHMA0000021696');
+
+  return(
+    <section className='test-response'>
+      <h2 data-cy='singleName'>{singleName}</h2>
+      <div data-cy='singleEncounter'>
+        <p>{encounter.description}</p>
+        <p>{encounter.name}</p>
+      </div>
+    </section>
+  )
 
 }
 
